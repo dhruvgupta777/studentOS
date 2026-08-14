39c67e5d8fdb392226b3cbd9c598b2685fc7229f
DHRUV GUPTA <dhruvgupta0107@gmail.com>
2026-08-14 23:49:27 +0530

    feat: add JWT token utility

---
+++ studentdashboard/src/main/java/com/studentdashboard/studentdashboard/StudentdashboardApplication.java
@@ -0,0 +1,19 @@
+package com.studentdashboard.studentdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class StudentdashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentdashboardApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}
